7d9778f2e12b255be53af9eccfcbf1fd3ccb2a0a
KaluginAleksey <mr.kamc@yandex.ru>
2022-06-15 13:00:56 +0300

    Добавил проверку на существование статьи

--- article.php
+++ article.php
@@ -4,7 +4,12 @@ require_once __DIR__ . '/autoload.php';
 
 if (isset($_GET['id']) & $_GET['id'] != '') {
     $article = \App\Article::findById($_GET['id']);
-
+    if (false != $article) {
+        include __DIR__ . '/templates/article.php';
+    } else {
+        header('Location: /php2/');
+        exit();
+    }
     include __DIR__ . '/templates/article.php';
 } else {
     header('Location: /php2/');
