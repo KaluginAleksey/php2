7b9299e7220664448fd569638979c5de39c8735d
KaluginAleksey <mr.kamc@yandex.ru>
2022-06-15 13:11:18 +0300

    Добавил проверку на существование статьи

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/autoload.php';
 
 $view = new \App\View();
-$view->news = \App\Article::findLast();
+$view->news = \App\Article::findLast(3);
 
 $view->display(__DIR__ . '/templates/index.php');
 
