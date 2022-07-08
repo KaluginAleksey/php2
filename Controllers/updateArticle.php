0f397fddcd37734f891b3b20deeaa4b5f863f48b
KaluginAleksey <mr.kamc@yandex.ru>
2022-07-08 09:26:36 +0300

    Исправление ошибок

--- Controllers/updateArticle.php
+++ Controllers/updateArticle.php
@@ -4,15 +4,17 @@ require_once __DIR__ . '/../autoload.php';
 
 if (!empty($_GET['id'])) {
     $article = \App\Article::findById($_GET['id']);
-    if ($article) {
+    if (false != $article) {
         $article->title = $_POST['title'];
         $article->text = $_POST['text'];
         $article->author = $_POST['author'];
         $article->date = $_POST['date'];
 
         $article->save();
+    } else {
+        header('Location:/php2/');
+        exit();
     }
-
 }
 
 header('Location:/php2/');
